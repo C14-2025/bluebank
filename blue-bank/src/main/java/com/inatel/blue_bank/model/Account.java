5525b7517e3fd113dd852a37cee0b5b075420152
Bruno-Fernands <bruno.bf@ges.inatel.br>
2025-10-08 11:43:13 -0300

    Creating Account class

--- blue-bank/src/main/java/com/inatel/blue_bank/model/Account.java
+++ blue-bank/src/main/java/com/inatel/blue_bank/model/Account.java
@@ -1,12 +1,55 @@
 package com.inatel.blue_bank.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "accounts")
+@Getter
+@Setter
+@ToString
+@EntityListeners(AuditingEntityListener.class)
 public class Account {
-    //TODO
 
-//    @OneToOne
-//    @JoinColumn(name = "customer_id", referencedColumnName = "id")
-//    private Customer customer;
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
 
-//    @OneToMany(mappedBy = "account")
-//    private List<Card> cards;
+    @Column(name = "account_number", length = 20, nullable = false, unique = true)
+    private String accountNumber;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "account_type", nullable = false)
+    private AccountType accountType;
+
+    @Column(name = "balance", nullable = false)
+    private BigDecimal balance;
+
+    @Column(name = "branch_code", length = 10, nullable = false)
+    private String branchCode;
+
+    @OneToOne
+    @JoinColumn(name = "customer_id", referencedColumnName = "id")
+    private Customer customer;
+
+    @OneToMany(mappedBy = "account")
+    private List<Card> cards;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
 }
---
+++ blue-bank/src/main/java/com/inatel/blue_bank/model/AccountType.java
@@ -0,0 +1,7 @@
+package com.inatel.blue_bank.model;
+
+public enum AccountType {
+    CHECKING,
+    SAVINGS,
+    INVESTMENT
+}

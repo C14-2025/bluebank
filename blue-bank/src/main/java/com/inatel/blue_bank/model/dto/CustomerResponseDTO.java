2c4db3b863f6323084ae07e38ce6adab411d08de
migstreva <miguelstreva@outlook.com>
2025-10-21 01:11:53 -0300

    Finished CustomerResponseDTO and added jackson timestamp config into application.yml file

--- blue-bank/src/main/java/com/inatel/blue_bank/model/dto/CustomerResponseDTO.java
+++ blue-bank/src/main/java/com/inatel/blue_bank/model/dto/CustomerResponseDTO.java
@@ -1,4 +1,21 @@
 package com.inatel.blue_bank.model.dto;
 
-public record CustomerResponseDTO() {
-}
+import com.inatel.blue_bank.model.DocType;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record CustomerResponseDTO(
+        UUID id,
+        String fullName,
+        LocalDate dob,
+        String nationality,
+        String phone,
+        String email,
+        String occupation,
+        DocType docType,
+        String docNumber,
+        LocalDateTime createdAt,
+        LocalDateTime updatedAt
+) {}

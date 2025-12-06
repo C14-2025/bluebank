e16be2b0458d8c473dbfdaf8de8ad94cc2acea86
migstreva <miguelstreva@outlook.com>
2025-12-06 19:24:46 -0300

    feat: add findByAccountNumberAndBranchCode to AccountService

--- apibluebank/blue-bank/src/main/java/com/inatel/blue_bank/controller/InvestmentController.java
+++ apibluebank/blue-bank/src/main/java/com/inatel/blue_bank/controller/InvestmentController.java
@@ -1,40 +1,58 @@
+package com.inatel.blue_bank.controller;
+
+import com.inatel.blue_bank.model.entity.Account;
+import com.inatel.blue_bank.model.entity.Investment;
+import com.inatel.blue_bank.service.AccountService;
+import com.inatel.blue_bank.service.InvestmentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 @RestController
 @RequestMapping("investments")
 @RequiredArgsConstructor
-public class InvestmentController implements GernericController {
-  private final InvestmentService service;
-  
-  @PostMapping
-  public ResponseEntity<Void> save(@RequestBody Investment investment){
-    service.save(investment);
-    URI location = generateHeaderLocation(invetment.getId());
-    return ResponseEntity.created(location).build();
-  }
+public class InvestmentController implements GenericController {
+    private final InvestmentService service;
+    private final AccountService accountService;
+
+    @PostMapping
+    public ResponseEntity<Void> save(@RequestBody Investment investment) {
+        service.save(investment);
+        URI location = generateHeaderLocation(investment.getId());
+        return ResponseEntity.created(location).build();
+    }
 
     @GetMapping("{id}")
     public ResponseEntity<Investment> getDetails(@PathVariable("id") String id) {
         UUID investmentId = UUID.fromString(id);
 
         return service
-                .findById(invetmentId)
-                .map(investment -> {
-                    return ResponseEntity.ok(investment);
-                })
+                .findById(investmentId)
+                .map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
-/*
+
     @GetMapping
     public ResponseEntity<List<Investment>> search(
             @RequestParam(value = "account-number", required = false) String accountNumber,
-            @RequestParam(value = "branch-code", required = false) String branchCode) {
-        .findById()
-        List<Investment> searchResult = service.searchByAccount(accountNumber, branchCode);
-        List<Investment> list = searchResult
-                .stream()
-                .map(authorMapper::toDTO).toList();
-        return ResponseEntity.ok(list);
+            @RequestParam(value = "branch-code", required = false) Integer branchCode) {
+
+        Optional<Account> accountOptional = accountService.findByAccountNumberAndBranchCode(accountNumber, branchCode);
+        if (accountOptional.isEmpty()) {
+            // Account not found
+            return ResponseEntity.notFound().build();
+        }
+
+        List<Investment> searchResult = service.searchByAccount(accountOptional.get());
+
+        return ResponseEntity.ok(searchResult);
     }
-*/
+
     @PutMapping("{id}")
     public ResponseEntity<Void> update(
             @PathVariable("id") String id, @RequestBody Investment investment) {
@@ -42,14 +60,14 @@ public class InvestmentController implements GernericController {
         UUID investmentId = UUID.fromString(id);
         Optional<Investment> investmentOptional = service.findById(investmentId);
 
-        if(investmentOptional.isEmpty()) {
+        if (investmentOptional.isEmpty()) {
             return ResponseEntity.notFound().build();
         }
 
         Investment investmentFound = investmentOptional.get();
-        investmentFound.setName(investment.getName);
-        investmentFound.setShare(investment.getShare);
-        investmentFound.setCostPerShare(investment.getCostPerShare);
+        investmentFound.setName(investment.getName());
+        investmentFound.setShare(investment.getShare());
+        investmentFound.setCostPerShare(investment.getCostPerShare());
 
         service.update(investmentFound);
 
@@ -61,7 +79,7 @@ public class InvestmentController implements GernericController {
         UUID investmentId = UUID.fromString(id);
         Optional<Investment> investmentOptional = service.findById(investmentId);
 
-        if(investmentOptional.isEmpty()) {
+        if (investmentOptional.isEmpty()) {
             return ResponseEntity.notFound().build();
         }
 
@@ -69,6 +87,4 @@ public class InvestmentController implements GernericController {
 
         return ResponseEntity.noContent().build();
     }
-}
-
 }
